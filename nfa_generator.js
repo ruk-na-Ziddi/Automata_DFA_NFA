69829eaad6ab7e77545fdec6b58e818759a14f60
Ankur Kumar Singh <aksinghmt@gmail.com>
2016-06-10 19:30:45 +0530

    Refactored nfa_generator and changed recursive function for epslon to <showedby Jayanth >

--- nfa_generator.js
+++ nfa_generator.js
@@ -16,79 +16,54 @@ var nfa_generator = function(set_of_states,alphabet_set, transition_function, in
 };
 
 nfa.prototype.accept = function(alphabets){
-	// console.log(this.runner(alphabets))
 	return lodash.intersection(this.set_of_final_states, lodash.flatten(this.runner(alphabets))).length >= 1;
 };
 
 nfa.prototype.runner = function(alphabets){
-	var self=this;
-	if(self.badRequest(alphabets))
+	var _this=this;
+	if(_this.badRequest(alphabets))
 		return ["bad state"];
 
 	if(alphabets == ""){
-		return self.continuousEplsonTransactions()(self.transition_function[self.initial_state][epslon_symbol]);
+		return _this.continuousEplsonTransactions([_this.initial_state]);
 	}
 
 	return alphabets.split("").reduce(function(states, alphabet){
-		return lodash.flatten(states).map(function(state){
-			var epslon_transaction = self.continuousEplsonTransactions()(self.transition_function[state][epslon_symbol] || []);
-			// console.log("epslon_transaction     ", epslon_transaction);
-
-			var next_to_epslon = self.getNextForEpslon(epslon_transaction, alphabet);
-			// console.log("next_to_epslon        ",next_to_epslon);
-
-			var epslon_transaction_from_last_transition = self.transition_function[state][alphabet] || []
-			// console.log("epslon_transaction_from_last_transition_before   ",epslon_transaction_from_last_transition)
-			if(alphabets.split("").lastIndexOf(alphabet) == alphabets.length-1)
-				epslon_transaction_from_last_transition = lodash.union(self.continuousEplsonTransactions()(self.transition_function[state][alphabet] || []), self.continuousEplsonTransactions()(next_to_epslon))
-			// console.log("epslon_transaction_from_last_transition     ",epslon_transaction_from_last_transition);
-
-			return lodash.flattenDeep(lodash.union(epslon_transaction_from_last_transition, next_to_epslon))
-		})
-	}, [self.initial_state]);
+		var possible_states_of_machine = _this.continuousEplsonTransactions(states);
+		var states_after_epslon_transaction = _this.alphabetTransactionOnStates(possible_states_of_machine, alphabet);
+		return _this.continuousEplsonTransactions(states_after_epslon_transaction);
+	}, [_this.initial_state]);
 };
 
 nfa.prototype.badRequest = function(alphabets){
-	var self=this;
+	var _this=this;
 	return alphabets.split("").some(function(ele){
-		return self.alphabet_set.indexOf(ele) == -1;
+		return _this.alphabet_set.indexOf(ele) == -1;
 	});
 }
 
-nfa.prototype.continuousEplsonTransactions = function(){
-	var self = this;
-	var all = [];
-	return function all_eps_transactions(states){
-		var result = [];
-		states.forEach(function(state){
-			if (!self.transition_function[state][epslon_symbol]) {self.transition_function[state][epslon_symbol] = []}
-			all.push(state);
-			result.push(self.transition_function[state][epslon_symbol]);
-		})
-
-		result = lodash.uniq(lodash.flattenDepth(result));
-		all.push(result);
-
-		var noEpslon = result.every(function(state){
-			if (!self.transition_function[state][epslon_symbol]) {self.transition_function[state][epslon_symbol] = []}
-			return self.transition_function[state][epslon_symbol].length == 0;
-		})
-
-		if(noEpslon)
-			return lodash.flattenDepth(all);
-		return all_eps_transactions(result);
-	}
-}
-
-nfa.prototype.getNextForEpslon = function(states, alphabet){
-	var self=this;
-	var result = [];
-	// console.log(states, alphabet);
+nfa.prototype.continuousEplsonTransactions = function(states){
+	var _this = this;
+	var next_candidates = []
 	states.forEach(function(state){
-		result.push(self.transition_function[state][alphabet] || [])
+		isUndefined(_this.transition_function[state]) && (_this.transition_function[state] = {epslon_symbol:[]})
+		next_candidates.push(_this.transition_function[state][epslon_symbol] || [])
 	})
-	return lodash.flattenDeep(lodash.uniq(lodash.flattenDepth(result)));
+	if(lodash.difference(lodash.flattenDeep(next_candidates), states).length == 0)
+		return states;
+	return _this.continuousEplsonTransactions(lodash.union(states, lodash.flattenDeep(next_candidates)))
 }
+
+nfa.prototype.alphabetTransactionOnStates = function(states, alphabet){
+	var _this=this;
+	var next_states_after_alphabet_transaction = [];
+	states.forEach(function(state){
+		next_states_after_alphabet_transaction.push(_this.transition_function[state][alphabet] || [])
+	})
+	return lodash.flattenDeep(lodash.uniq(lodash.flattenDeep(next_states_after_alphabet_transaction)));
+}
+
+function isUndefined(element){ return element == undefined };
 
 exports.nfa_generator = nfa_generator;
 // ε
